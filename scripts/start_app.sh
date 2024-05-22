f930f856183f0368ed74f8ff478d27591ff14208
abko27700 <abko27700@gmail.com>
2024-05-22 12:48:39 -0400

    working start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-# Change to the project directory
 cd /home/ec2-user/projectDaria/smartCalendar
 
-# Install Pipenv if not already installed
-python3 -m ensurepip --upgrade
-python3 -m pip install --user pipenv
+# Install dependencies using pip
+pip install flask google-auth google-auth-oauthlib google-api-python-client
 
-# Add Pipenv to the PATH
-export PATH="/home/ec2-user/.local/bin:$PATH"
-
-# Install project dependencies using Pipenv
-pipenv install --deploy
-
-# Start the application using Gunicorn
-pipenv run nohup gunicorn -w 1 -b 0.0.0.0:5000 app:app &
+# Start the application (assuming app.py with app function)
+nohup gunicorn -w 2 -b 0.0.0.0:5000 app:app &
