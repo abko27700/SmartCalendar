6cb727dcb957f218fc729069e5bf5b297f3aee52
Abhishek Kothari <51488404+abko27700@users.noreply.github.com>
2024-05-23 11:07:03 -0400

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -5,7 +5,8 @@ cd /home/ec2-user/projectDaria/smartCalendar
 sh /home/ec2-user/smartCalendarCommands.sh
 
 # Install dependencies using pip
-pip install flask google-auth google-auth-oauthlib google-api-python-client
+pip install flask google-auth google-auth-oauthlib google-api-python-client gunicorn
+
 
 # Start the application (assuming app.py with app function)
-nohup ~/.local/bin/gunicorn -w 2 -b 0.0.0.0:5000 app:app >> /home/ec2-user/logs/smartCalendar.log 2>&1 &
+nohup gunicorn -w 2 -b 0.0.0.0:5000 app:app >> /home/ec2-user/logs/smartCalendar.log 2>&1 &
